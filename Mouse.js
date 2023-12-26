ef9a1cc4981a389a2bc080a182d52c12299112f3
Tykes2021 <114346683+Tykes2021@users.noreply.github.com>
2023-12-26 11:11:02 +0530

    Add files via upload

---
+++ Mouse.js
@@ -0,0 +1,88 @@
+Status = "";
+
+Mouse_image = "";
+
+objects = [];
+
+function preload()
+
+{
+
+    Mouse_image = loadImage("Mouse.jpg");
+
+}
+
+function setup()
+
+{
+
+    canvas = createCanvas(600,350);
+
+    canvas.position(475,200);
+
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded()
+
+{
+
+    console.log("Model Loaded!");
+
+    Status = true;
+
+    object_Detector.detect(Mouse_image,gotResults);
+
+}
+
+function gotResults(error,results)
+
+{
+
+    if(error)
+    
+    {
+
+        console.error(error);
+
+    }
+
+    console.log(results);
+
+}
+
+function draw()
+
+{
+
+    image(Mouse_image,0,0,640,350);
+
+    if(Status != "")
+    
+    {
+
+        for(i = 0; i < objects.length; i++)
+        
+        {
+
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+
+            percent = floor(objects[i].confidence * 100);
+
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+           
+            noFill();
+           
+            stroke("#fc0303");
+           
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+      
+        }
+   
+    }
+}
